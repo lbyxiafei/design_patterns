7441eb318951ce9a07ec001b8fdbdcbac50cac69
Binyan Li <binyanli@microsoft.com>
2023-01-08 21:00:18 -0800

    wrap up singleton.

--- SingletonPattern/main.cpp
+++ SingletonPattern/main.cpp
@@ -6,15 +6,14 @@
 using namespace std;
 
 int main(){
-    cout << "hello" << endl;
+    cout << "pointer as singleton:" << endl;
     auto pptr = SingletonPtr::getInstance();
     pptr->setCnt(5);
     auto pptr2 = SingletonPtr::getInstance();
     pptr2->setCnt(555);
     cout << pptr->getCnt() << ',' << pptr2->getCnt() << endl << endl;
 
-    cout << "hello" << endl;
-    // instance version
+    cout << "instance version:" << endl;
     auto ptr = make_shared<Singleton>(Singleton::getInstance());
     ptr->setCnt(1);
     auto ptr2 = make_shared<Singleton>(Singleton::getInstance());
